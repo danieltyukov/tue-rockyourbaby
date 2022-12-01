40b997e16c1a2cb2814bf11693f3ea55166c9cf1
Daniel Tyukov <60662998+danieltyukov@users.noreply.github.com>
2022-12-01 23:00:14 +0100

    Update heartbeat_input.ino

--- heartbeat_input.ino
+++ heartbeat_input.ino
@@ -1,17 +1,31 @@
 
+//#include <M5Stack.h>
+
 // 3.3V
 // GND
-// GIPIO 36
-#define LIGHT_SENSOR_PIN 36
+// GPIO 36 - FOR ANALOG
+// GPIO 5 - FOR DIGITAL
+#define ANALOG_SENSOR_PIN 36
+#define DIGITAL_SENSOR_PIN  5
 
 void setup() {
+  // M5.begin();
   // serial communication at 9600 bits/s
   Serial.begin(9600);
+  pinMode(36, INPUT);
+  pinMode(5, INPUT);
 }
 
 void loop() {
-  int analogValue = analogRead(LIGHT_SENSOR_PIN);
-  Serial.print("Analog Value = ");
-  Serial.print(analogValue);
+  int analogValue = analogRead(ANALOG_SENSOR_PIN);
+  int digitalValue = digitalRead(DIGITAL_SENSOR_PIN);
+  Serial.println("Analog Value = ");
+  Serial.println(analogValue);
+
   delay(500);
+
+  Serial.println("Digital Value = ");
+  Serial.println(digitalValue);
+
+  delay(900);
 }
