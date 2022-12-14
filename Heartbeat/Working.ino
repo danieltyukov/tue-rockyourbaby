2b02c000c1124aa35008d907e9efde0fac78d734
Lucas <78037063+LucasVervoort@users.noreply.github.com>
2022-12-14 16:52:03 +0100

    Update Working.ino

--- Heartbeat/Working.ino
+++ Heartbeat/Working.ino
@@ -14,11 +14,11 @@ void setup() {
 
 void loop() {
 
-  buttonState = digitalRead(ButtonPin);
+  buttonState = digitalRead(ButtonPin); //Read the state of the button, only for testing
 
   //Check if the button is pressed, if it is the buttonstate is HIGH:
   if (buttonState == HIGH){
-    Serial.print("Button is high");
+    Serial.print("Button is high"); 
     previous_value = digitalRead(LDRpin); //For the first time we run the loop we don't have a accurate value, so we need to set the previous value one time.
 //    Serial.print("Previous value is: ");
 //    Serial.println(previous_value);
@@ -26,38 +26,38 @@ void loop() {
     Serial.print("BPM: ");
     Serial.println(BPM);              // Here the actual value gets printed
   }  
-  delay(100);
+  delay(100); //Small delay not neccessary
 }
 
 int currentBPM(){
-  int first_change = 0; 
+  int first_change = 0;   //Boolean integers to stop loops
   int second_change = 0;
 
-  int timestart = 0; //Variable to see time
-  int timestop = 0; //Variable to see time
+  int timestart = 0; //Variable to see start time
+  int timestop = 0; //Variable to see stopping time
   int timepassed = 0; //To store the time the has passed between the two variable above
-  int beats = 1;
+  int beats = 1; //Beats, this is automatically 0, because 
 
-  int measuring_value = 0;
-  previous_value = digitalRead(LDRpin);     // read the value from the LDR
+  int measuring_value = 0; //Variable that isn't used anymore, it think.
+  previous_value = digitalRead(LDRpin);     // read the value from the LDR, 
 
 //  Serial.print("Initialized");
   while(first_change == 0){    //While loop which stop if there is a bpm
     LDRValue = digitalRead(LDRpin);     // read the value from the LDR
     Serial.println(LDRValue);
 //    Serial.print("first time looping");
-    if(LDRValue != previous_value){
+    if(LDRValue != previous_value){  //Checks if there has been a change from last loop
       timestart = millis();  //Takes the time the first change is noticed
       Serial.print("First change!");
       Serial.print("Previous: ");
       Serial.println(previous_value);
-      first_change = 1;
+      first_change = 1; //Boolean variable, not necessarry, when coding smart 
     }
     previous_value = LDRValue; // Set the previous vale to the current loop for next loop
   }
   
   while(second_change == 0){
-    LDRValue = digitalRead(LDRpin);
+    LDRValue = digitalRead(LDRpin); // read the value from the LDR
     Serial.println(LDRValue);
     if(LDRValue != previous_value){     // Checks of the hearbeat has changed from last time
       timestop = millis();   //Takes the time the second cange is noticed
@@ -67,13 +67,13 @@ int currentBPM(){
       Serial.print("Timepassed: ");
       Serial.println(timepassed);
 
-      if(timepassed > 500){
+      if(timepassed > 500){ //If start
         Serial.print("beats");
         Serial.println(beats);
         BPM = ((0.5 *60*beats)/(0.001*timepassed)); //Calculate the current bpm which is time two, because you got half a cycle, times 0.001 because the time is in millis and times 60 because it is beats per MINUTE
   //      Serial.print("BPM is: ");
   //      Serial.println(BPM);
-        second_change = 1;
+        second_change = 1;  //Boolean variable, not necessarry, when coding smart
       }
       else{
         beats = beats+1;
